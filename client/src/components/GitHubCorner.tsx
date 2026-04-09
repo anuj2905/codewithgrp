72af5654f4c04c216fa975f836538937aeae9038
Anuj Patil <patilanuj2909@gmail.com>
2026-04-09 22:55:23 +0530

    Update GitHub link in GitHubCorner component

--- client/src/components/GitHubCorner.tsx
+++ client/src/components/GitHubCorner.tsx
@@ -7,7 +7,7 @@ function GitHubCorner() {
 
     return (
         <a
-            href="https://github.com/sahilatahar/Code-Sync"
+            href="https://codewithgrp.vercel.app/"
             className="github-corner"
             aria-label="View source on GitHub"
             target="_blank"
